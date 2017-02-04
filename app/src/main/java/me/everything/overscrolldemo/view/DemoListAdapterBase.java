c22bf5140d27d949c25f5547c8573e967333b46b
Amit Davidi <amit@everything.me>
2017-02-04 21:52:55 +0200

    Add click listeners to better inspect potential issues

--- app/src/main/java/me/everything/overscrolldemo/view/DemoListAdapterBase.java
+++ app/src/main/java/me/everything/overscrolldemo/view/DemoListAdapterBase.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,6 +24,19 @@ public abstract class DemoListAdapterBase extends BaseAdapter {
 
         public DemoViewHolder(int resId, ViewGroup parent, LayoutInflater inflater) {
             mTextView = (TextView) inflater.inflate(resId, parent, false);
+            mTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mTextView.getContext(), "Tapped on: "+mTextView.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            mTextView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(mTextView.getContext(), "Long-tapped on: "+mTextView.getText(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
         }
 
         public void init(DemoItem item) {
--- app/src/main/java/me/everything/overscrolldemo/view/DemoRecyclerAdapterBase.java
+++ app/src/main/java/me/everything/overscrolldemo/view/DemoRecyclerAdapterBase.java
@@ -2,8 +2,10 @@ package me.everything.overscrolldemo.view;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,6 +22,20 @@ public abstract class DemoRecyclerAdapterBase extends RecyclerView.Adapter<Recyc
 
         public DemoViewHolder(int resId, ViewGroup parent, LayoutInflater inflater) {
             super(inflater.inflate(resId, parent, false));
+
+            getTextView().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getTextView().getContext(), "Tapped on: "+getTextView().getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            getTextView().setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(getTextView().getContext(), "Long-tapped on: "+getTextView().getText(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
         }
 
         public void init(DemoItem item) {
